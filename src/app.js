70ce940c56487da8d76c19c4077a11305c7726e1
an-rybacka <121604351+an-rybacka@users.noreply.github.com>
2023-01-13 13:10:45 +0000

    Js Code simplified

--- src/app.js
+++ src/app.js
@@ -1,19 +1,10 @@
 function showTime(event) {
-
-  if (event.target.value === "Lisbon") {
-    alert(`It is ${moment()
-    .tz("Europe/Lisbon")
-    .format("dddd, MMMM D, YYYY HH:mm A")} in Lisbon, Portugal`);
-  }
-  if (event.target.value === "Tokyo") {
-    alert(`It is ${moment()
-    .tz("Asia/Tokyo")
-    .format("dddd, MMMM D, YYYY HH:mm A")} in Tokyo, Japan`);
-  }
-  if (event.target.value === "Bogota") {
-    alert(`It is ${moment()
-    .tz("America/Bogota")
-    .format("dddd, MMMM D, YYYY HH:mm A")} in Bogota, Colombia`);
+  if (event.target.value.length > 0) {
+    alert(
+      `It is ${moment()
+        .tz(event.target.value)
+        .format("dddd, MMMM D, YYYY HH:mm A")} in ${event.target.value}`
+    );
   }
 }
 
