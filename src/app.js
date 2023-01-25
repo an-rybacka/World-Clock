055f14a43e743f2fe3c01ab38f45c6e82dac85c1
an-rybacka <121604351+an-rybacka@users.noreply.github.com>
2023-01-25 17:26:19 +0100

    JS - added multiple locations
    and connecting Select with functions

--- src/app.js
+++ src/app.js
@@ -1,36 +1,62 @@
 function updateTime() {
   //Lisbon
   let lisbonElement = document.querySelector("#lisbon");
-  let lisbonDateElement = lisbonElement.querySelector(".date");
-  let lisbonTimeElement = lisbonElement.querySelector(".time");
-  let lisbonTime = moment().tz("Europe/Lisbon");
+  if (lisbonElement) {
+    let lisbonDateElement = lisbonElement.querySelector(".date");
+    let lisbonTimeElement = lisbonElement.querySelector(".time");
+    let lisbonTime = moment().tz("Europe/Lisbon");
 
-  lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
-  lisbonTimeElement.innerHTML = `${lisbonTime.format(
-    "h:mm:ss"
-  )} <small>${lisbonTime.format("A")}</small>`;
+    lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+    lisbonTimeElement.innerHTML = `${lisbonTime.format(
+      "h:mm:ss"
+    )} <small>${lisbonTime.format("A")}</small>`;
+  }
 
   //Tokyo
   let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDateElement = tokyoElement.querySelector(".date");
-  let tokyoTimeElement = tokyoElement.querySelector(".time");
-  let tokyoTime = moment().tz("Asia/Tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
 
-  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-  tokyoTimeElement.innerHTML = `${tokyoTime.format(
-    "h:mm:ss"
-  )} <small>${tokyoTime.format("A")}</small>`;
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = `${tokyoTime.format(
+      "h:mm:ss"
+    )} <small>${tokyoTime.format("A")}</small>`;
+  }
 
   //Bogota
   let bogotaElement = document.querySelector("#bogota");
-  let bogotaDateElement = bogotaElement.querySelector(".date");
-  let bogotaTimeElement = bogotaElement.querySelector(".time");
-  let bogotaTime = moment().tz("America/Bogota");
+  if (bogotaElement) {
+    let bogotaDateElement = bogotaElement.querySelector(".date");
+    let bogotaTimeElement = bogotaElement.querySelector(".time");
+    let bogotaTime = moment().tz("America/Bogota");
 
-  bogotaDateElement.innerHTML = bogotaTime.format("MMMM Do YYYY");
-  bogotaTimeElement.innerHTML = `${bogotaTime.format(
-    "h:mm:ss"
-  )} <small>${bogotaTime.format("A")}</small>`;
+    bogotaDateElement.innerHTML = bogotaTime.format("MMMM Do YYYY");
+    bogotaTimeElement.innerHTML = `${bogotaTime.format(
+      "h:mm:ss"
+    )} <small>${bogotaTime.format("A")}</small>`;
+  }
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+        <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYY")}</div>
+        </div>
+       <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
